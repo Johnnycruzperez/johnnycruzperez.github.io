ee2c8cbfe99efe4139138f2bac0d6f5adededd76
Johnnycruzperez <126531817+Johnnycruzperez@users.noreply.github.com>
2023-03-05 20:34:56 -0300

    Reset formulario contacto

---
+++ formulario.js
@@ -0,0 +1,8 @@
+function envio() {
+    document.getElementById("nombre").value = ""
+    document.getElementById("apellido").value = ""
+    document.getElementById("asunto").value = ""
+    return false;
+}
+
+
