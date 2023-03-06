4c7d466bce7007ef216dbccee5dedb3942327497
Johnnycruzperez <126531817+Johnnycruzperez@users.noreply.github.com>
2023-03-05 21:16:46 -0300

    boyon envio contacto visible/invisible

--- formulario.js
+++ formulario.js
@@ -2,7 +2,18 @@ function envio() {
     document.getElementById("nombre").value = ""
     document.getElementById("apellido").value = ""
     document.getElementById("asunto").value = ""
+
+
+    document.getElementById("enviado").style = "visibility: visible"
+    window.setTimeout(borrado_envio, 1500)
+
+
+
+
     return false;
 }
 
+function borrado_envio() {
+    document.getElementById("enviado").style = ""
 
+}
