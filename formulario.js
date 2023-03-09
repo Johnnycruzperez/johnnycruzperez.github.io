5aa5b4ebf18f44cd56943b7b525357ff690fb5d6
Johnnycruzperez <126531817+Johnnycruzperez@users.noreply.github.com>
2023-03-09 12:46:39 -0300

    Cambio apellido por email en formulario de contacto

--- formulario.js
+++ formulario.js
@@ -1,6 +1,6 @@
 function envio() {
     document.getElementById("nombre").value = ""
-    document.getElementById("apellido").value = ""
+    document.getElementById("email").value = ""
     document.getElementById("asunto").value = ""
     document.getElementById("enviado").style = "visibility: visible"
     window.setTimeout(borrado_envio, 1500)
